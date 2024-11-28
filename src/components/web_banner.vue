aa02fe4059f854a0e67f6637d6e5c65c7822e5b2
chiheng <chiheng357@gmail.com>
2024-11-28 19:31:18 +0700

    Update collectible page and web banner

--- src/components/web_banner.vue
+++ src/components/web_banner.vue
@@ -2,8 +2,8 @@
     <div class="webbannerContainer">
         <img class="img" :src="images[currentIndex]" alt="image">
         <div class="btnContainer">
-            <button @click="showPrevious()" class="previous"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"></path></svg></button>
-            <button @click="showNext()" class="next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M13.1717 12.0007L8.22192 7.05093L9.63614 5.63672L16.0001 12.0007L9.63614 18.3646L8.22192 16.9504L13.1717 12.0007Z"></path></svg></button>
+            <button @click="showPrevious()" class="btn"><svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"></path></svg></button>
+            <button @click="showNext()" class="btn"><svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M13.1717 12.0007L8.22192 7.05093L9.63614 5.63672L16.0001 12.0007L9.63614 18.3646L8.22192 16.9504L13.1717 12.0007Z"></path></svg></button>
         </div>
         <div class="indicator">
             <span v-for="(image, index) in images" :key="index" :class="['dot', { active: index === currentIndex }]"></span>
@@ -66,7 +66,7 @@ export default {
     align-items: center;
     justify-content: space-between;
 }
-.next{
+.btn{
     color: black;
     background-color: white;
     opacity: 50%;
@@ -74,21 +74,19 @@ export default {
     height: 50px;
     border-radius: 50%;
     cursor: pointer;
+    border: none;  
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
 }
-.previous{
-    color: black;
-    background-color: white;
-    opacity: 50%;
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    cursor: pointer;
+.btn:hover{
+    transform: translateY(-5px);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2); 
+    opacity: 70%;
 }
 .indicator {
     position: absolute;
     display: flex;
     margin-bottom: 50px;
-
 }
 .dot {
     height: 10px;
--- src/views/CollectiblesPage.vue
+++ src/views/CollectiblesPage.vue
@@ -1,3 +1,51 @@
 <template>
-  <h1>hi this a page</h1>
+  <web_banner :images="ImageList"/>
+  <div class="sectionOne"> 
+    <product_card v-for="(productCard, index) in productCard"
+      :key="index"
+      :productImage="productCard.productImage"
+      :title="productCard.title"
+      :status="productCard.status"/>
+  </div>
 </template>
+
+<script>
+import web_banner from '@/components/web_banner.vue';
+import product_card from '@/components/product_card.vue';
+export default{
+  components:{
+    web_banner,
+    product_card
+  },
+  data(){
+    return{
+      ImageList: [        
+        './src/assets/images/aespa3.jpg',
+        './src/assets/images/aespa4.jpg',
+        './src/assets/images/newjeans.jpg',
+        './src/assets/images/nct.jpg',
+        './src/assets/images/illit.jpg',
+        './src/assets/images/lessera.jpg',
+        './src/assets/images/RIIZE.jpeg',
+        './src/assets/images/straykids.jpg',
+        './src/assets/images/ateez.jpeg',
+      ],
+      productCard:[
+        {productImage: './src/assets/images/testimg1.jpg', title: 'Hello Bomboclat', status: 'Available'},
+        {productImage: './src/assets/images/testimg1.jpg', title: 'Hello Bomboclat', status: 'Available'},
+        {productImage: './src/assets/images/testimg1.jpg', title: 'Hello Bomboclat', status: 'Available'},
+        {productImage: './src/assets/images/testimg1.jpg', title: 'Hello Bomboclat', status: 'Available'},
+      ]
+    }
+
+  }
+}
+</script>
+
+<style>
+.sectionOne{
+  display: flex;
+  width: 100%;
+  overflow: auto
+}
+</style>
